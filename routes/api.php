76927f2cf859a298879344f34881cf44f30be823
luanamoarisf <luanaflorenciopc@gmail.com>
2024-08-12 15:46:31 -0300

    route fix2

--- routes/api.php
+++ routes/api.php
@@ -18,4 +18,6 @@ use App\Http\Controllers\BookController;
 // Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 //     return $request->user();
 // });
-Route::post('/registerbook', 'BookCntroller@store');
+Route::resource('book', BookController::class)->only([
+    'store'
+]);
